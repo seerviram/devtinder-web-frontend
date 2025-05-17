d9ab729053e486f89579efbee58fae0d28363b97
Bholaram <mailtoseervi141@gmail.com>
2025-05-17 19:57:01 +0530

    login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from "./login";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-[50px]'>Hello world
-       </div>
+      <div className='text-[50px]'>Hello world</div>
+      <Login/>
     </>
   )
 }
